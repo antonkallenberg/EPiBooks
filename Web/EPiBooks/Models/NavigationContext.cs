37e033e198e9189e2a9c3eef9d61af01fc756aa1
anton kallenberg <anton.kallenberg@gmail.com>
2012-07-23 15:38:35 +0200

    adding selected item

--- Web/EPiBooks/Models/NavigationContext.cs
+++ Web/EPiBooks/Models/NavigationContext.cs
@@ -8,13 +8,22 @@ using EPiServer.ServiceLocation;
 namespace EPiBooks.Models {
     public class NavigationContext {
         public PageData Item { get; private set; }
+        public ContentReference Selected { get; set; }
 
-        public IEnumerable<NavigationContext> GetItems(PageReference parent) {
+        public IEnumerable<NavigationContext> GetItems(PageReference parent, ContentReference selected, bool includeParent) {
+            if (PageReference.IsNullOrEmpty(parent)) {
+                return Enumerable.Empty<NavigationContext>();
+            }
+
             var contentLoader = ServiceLocator.Current.GetInstance<IContentLoader>();
-            var firstLevel = FilterForVisitor.Filter(new PageDataCollection(contentLoader.GetChildren<PageData>(parent)));
-            return firstLevel.Select(x => new NavigationContext { Item = x });
+            var firstLevel = new List<PageData>();
+            if (includeParent) {
+                firstLevel.Add(contentLoader.Get<PageData>(parent));
+            }
+            firstLevel.AddRange(FilterForVisitor.Filter(new PageDataCollection(contentLoader.GetChildren<PageData>(parent))));
+            return firstLevel.Select(x => new NavigationContext { Item = x, Selected = selected });
         }
 
-        public bool IsSelected { get { return false; } }
+        public bool IsSelected { get { return Item.PageLink.CompareToIgnoreWorkID(Selected); } }
     }
 }
