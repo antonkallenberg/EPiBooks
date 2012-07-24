3f091b4c0bdf94deb9833812062376466abfd2ab
anton kallenberg <anton.kallenberg@gmail.com>
2012-07-24 15:00:37 +0200

    adding verifications

--- EPiBooks.Tests/LatestBooksControllerSpecs.cs
+++ EPiBooks.Tests/LatestBooksControllerSpecs.cs
@@ -13,8 +13,9 @@ using It = Machine.Specifications.It;
 using ItIs = Moq.It;
 
 namespace EPiBooks.Tests {
+    [Subject("LatestBooksControllerSpecs")]
     public class LatestBooksControllerSpecs {
-        [Subject("LatestBooksControllerSpec")]
+
         public class heading_is_returned_to_view {
 
             private static readonly Mock<IContentRepository> ContentRepositoryMock = new Mock<IContentRepository>();
@@ -23,7 +24,7 @@ namespace EPiBooks.Tests {
             private Establish setup = () => {
                 LatestBooksBlockMock.SetupProperty(x => x.BookRoot, new PageReference(5));
                 LatestBooksBlockMock.SetupProperty(x => x.Heading, "latest books");
-                ContentRepositoryMock.Setup(x => x.GetChildren<BookPage>(LatestBooksBlockMock.Object.BookRoot)).Returns(new List<BookPage>());
+                ContentRepositoryMock.Setup(x => x.GetChildren<BookPage>(LatestBooksBlockMock.Object.BookRoot, ItIs.IsAny<ILanguageSelector>(), 0, 5)).Returns(new List<BookPage>());
             };
 
             private static LatestBooksViewModel viewModel;
@@ -33,15 +34,19 @@ namespace EPiBooks.Tests {
                 viewModel = view.Model as LatestBooksViewModel;
             };
 
-            private It title_is_passed_to_view = () => viewModel.LatestBooksBlock.Heading.Equals("latest books", StringComparison.OrdinalIgnoreCase);
+            private It title_is_passed_to_view = () => {
+                viewModel.LatestBooksBlock.Heading.Equals("latest books", StringComparison.OrdinalIgnoreCase);
+                ContentRepositoryMock.Verify(x => x.GetChildren<BookPage>(LatestBooksBlockMock.Object.BookRoot, ItIs.IsAny<ILanguageSelector>(), 0, 5), Times.Once());
+            };
         }
 
-        public class empty_book_list_is_retured
-        {
+        public class empty_book_list_is_retured {
             private static readonly Mock<IContentRepository> ContentRepositoryMock = new Mock<IContentRepository>();
             private static readonly Mock<LatestBooksBlock> LatestBooksBlockMock = new Mock<LatestBooksBlock>();
-            
-            private Establish setup = () => LatestBooksBlockMock.SetupProperty(x => x.BookRoot, PageReference.EmptyReference);
+
+            private Establish setup = () => {
+                LatestBooksBlockMock.SetupProperty(x => x.BookRoot, PageReference.EmptyReference);
+            };
 
             private static LatestBooksViewModel viewModel;
             private Because index_is_execuded_without_a_book_folder_root = () => {
@@ -50,7 +55,10 @@ namespace EPiBooks.Tests {
                 viewModel = view.Model as LatestBooksViewModel;
             };
 
-            private It latest_books_should_be_empty = () => viewModel.LatestBooks.ShouldBeEmpty();
+            private It latest_books_should_be_empty = () => {
+                viewModel.LatestBooks.ShouldBeEmpty();
+                ContentRepositoryMock.Verify(x => x.GetChildren<BookPage>(ItIs.IsAny<ContentReference>()), Times.Never());
+            };
         }
     }
 }
