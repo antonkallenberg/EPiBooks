84fa52cf1281e4e0b26d7eb1785646c644814532
anton kallenberg <anton.kallenberg@gmail.com>
2012-08-10 16:00:58 +0200

    try catch with exit code

--- Build.ps1
+++ Build.ps1
@@ -6,13 +6,19 @@ param(
 )
 
 function Build() {	
-	if($Environment -ieq 'debug') {
-		.\Web\EPiBooks\Tools\psake.ps1 ".\Web\EPiBooks\BuildScripts\Deploy.ps1" -properties @{ config='debug'; environment="$Environment" }
+	Try {
+		if($Environment -ieq 'debug') {
+			.\Web\EPiBooks\Tools\psake.ps1 ".\Web\EPiBooks\BuildScripts\Deploy.ps1" -properties @{ config='debug'; environment="$Environment" }
+		}
+		if($Environment -ieq 'production') {
+			.\Web\EPiBooks\Tools\psake.ps1 ".\Web\EPiBooks\BuildScripts\Deploy.ps1" -properties @{ config='release'; environment="$Environment"; deployToFtp = $DeployToFtp } "production"
+		}
+		Write-Host "$Environment build done!"
 	}
-	if($Environment -ieq 'production') {
-		.\Web\EPiBooks\Tools\psake.ps1 ".\Web\EPiBooks\BuildScripts\Deploy.ps1" -properties @{ config='release'; environment="$Environment"; deployToFtp = $DeployToFtp } "production"
+	Catch
+	{
+		Exit 1;
 	}
-	Write-Host "$Environment build done!"
 }
 
 Build
